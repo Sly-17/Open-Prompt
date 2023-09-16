30cb118da92a1aa9281419565aadd7ade5b535c3
Sly-17 <98656858+Sly-17@users.noreply.github.com>
2023-09-16 12:42:04 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 #! /bin/bash
 
 echo $(gcc -o promptgpt ./.src/prompt.c -lcurl)
+echo "Build Successful !!  Prompt using the following command : ./promptgpt \"<Your prompt>\"";
+echo "Occasional Segmentation Faults may occur, but just try re-running the command in such cases";
+echo "Do not overwhelm the server with too many prompts";
